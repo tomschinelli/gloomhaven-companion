1a1dd4107b7e30b9042bcfe8441509c20c5b39d5
Tom Schinelli <tom.schinelli@gmail.com>
2023-06-15 23:41:54 +0200

    Add full pwa requirements

--- src/service-worker.js
+++ src/service-worker.js
@@ -30,47 +30,33 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(deleteOldCaches());
 });
 
-/**
- * Fetch the asset from the network and store it in the cache.
- * Fall back to the cache if the user is offline.
- */
-async function fetchAndCache(request) {
-    const cache = await caches.open(`offline${timestamp}`);
-
-    try {
-        const response = await fetch(request);
-        cache.put(request, response.clone());
-        return response;
-    } catch (err) {
-        const response = await cache.match(request);
-        if (response) return response;
-
-        throw err;
-    }
-}
-
 self.addEventListener('fetch', (event) => {
-    if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
+    // ignore POST requests etc
+    if (event.request.method !== 'GET') return;
 
-    const url = new URL(event.request.url);
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
 
-    // don't try to handle e.g. data: URIs
-    const isHttp = url.protocol.startsWith('http');
-    const isDevServerRequest =
-        url.hostname === self.location.hostname && url.port !== self.location.port;
-    const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
-    const skipBecauseUncached = event.request.cache === 'only-if-cached' && !isStaticAsset;
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            return cache.match(url.pathname);
+        }
 
-    if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
-        event.respondWith(
-            (async () => {
-                // always serve static files and bundler-generated assets from cache.
-                // if your application has other URLs with data that will never change,
-                // set this variable to true for them and they will only be fetched once.
-                const cachedAsset = isStaticAsset && (await caches.match(event.request));
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
 
-                return cachedAsset || fetchAndCache(event.request);
-            })()
-        );
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch {
+            return cache.match(event.request);
+        }
     }
+
+    event.respondWith(respond());
 });
