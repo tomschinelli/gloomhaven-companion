a07dcbf17401ec9290ba45aba8ff8a7c73632227
Tom Schinelli <tom.schinelli@gmail.com>
2023-06-17 21:27:02 +0200

    Add ssl to loadbalancer

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,11 +1,3 @@
-#module "gloomhaven_companion" {
-#  source                = "./modules/hosted_zone_delegation"
-#  domain_name           = var.domain_name
-#  cloudflare_account_id = var.cloudflare_account_id
-#  cloudflare_zone       = var.cloudflare_zone
-#}
-
-
 # ---- networking ---- #
 
 resource "aws_vpc" "vpc" {
@@ -205,7 +197,7 @@ resource "aws_ecs_service" "service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    target_group_arn = aws_alb_target_group.target_group.arn # Referencing our target group
     container_name   = aws_ecs_task_definition.task_definition.family
     container_port   = 3000 # Specifying the container port
   }
@@ -238,12 +230,14 @@ resource "aws_security_group" "service_security_group" {
   }
 }
 
+# ---- ALB ---- #
+
 resource "aws_security_group" "load_balancer_security_group" {
   vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port        = 80
-    to_port          = 80
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -275,7 +269,7 @@ resource "aws_alb" "application_load_balancer" {
   }
 }
 
-resource "aws_lb_target_group" "target_group" {
+resource "aws_alb_target_group" "target_group" {
   name        = "${var.app_name}-${var.app_environment}-tg"
   port        = 80
   protocol    = "HTTP"
@@ -284,7 +278,7 @@ resource "aws_lb_target_group" "target_group" {
 
   health_check {
     matcher = "200,301,302"
-    path = "/"
+    path    = "/"
   }
 
   tags = {
@@ -293,13 +287,75 @@ resource "aws_lb_target_group" "target_group" {
   }
 }
 
-resource "aws_lb_listener" "listener" {
+resource "aws_alb_listener" "listener" {
   load_balancer_arn = aws_alb.application_load_balancer.id
-  port              = "80"
-  protocol          = "HTTP"
-
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn = aws_acm_certificate.gloomhaven_companion.arn
+  ssl_policy = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.id
+    target_group_arn = aws_alb_target_group.target_group.id
+  }
+}
+
+
+# ---- DNS ---- #
+
+resource "aws_route53_zone" "gloomhaven_companion" {
+  name = var.domain_name
+}
+
+data "cloudflare_zone" "gloomhaven_companion" {
+  account_id = var.cloudflare_account_id
+  name       = var.cloudflare_zone
+
+
+}
+resource "cloudflare_record" "gloomhaven_companion" {
+  count   = 4
+  zone_id = data.cloudflare_zone.gloomhaven_companion.id
+  name    = var.domain_name
+  value   = element(aws_route53_zone.gloomhaven_companion.name_servers, count.index)
+  depends_on = [
+    aws_route53_zone.gloomhaven_companion
+  ]
+  type = "NS"
+
+}
+
+
+resource "aws_acm_certificate" "gloomhaven_companion" {
+  domain_name       = aws_route53_record.app.fqdn
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_route53_record" "cert_validation" {
+  allow_overwrite = true
+  name            = tolist(aws_acm_certificate.gloomhaven_companion.domain_validation_options)[0].resource_record_name
+  records         = [
+    tolist(aws_acm_certificate.gloomhaven_companion.domain_validation_options)[0].resource_record_value
+  ]
+  type            = tolist(aws_acm_certificate.gloomhaven_companion.domain_validation_options)[0].resource_record_type
+  zone_id         = aws_route53_zone.gloomhaven_companion.id
+  ttl             = 60
+}
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.gloomhaven_companion.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+
+
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.gloomhaven_companion.zone_id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name                   = aws_alb.application_load_balancer.dns_name
+    zone_id                = aws_alb.application_load_balancer.zone_id
+    evaluate_target_health = false
   }
 }
--- deployment/modules/hosted_zone_delegation/main.tf
+++ deployment/modules/hosted_zone_delegation/main.tf
@@ -7,21 +7,3 @@ terraform {
   }
 }
 
-
-resource "aws_route53_zone" "gloomhaven_companion" {
-  name = var.domain_name
-}
-
-data "cloudflare_zone" "gloomhaven_companion" {
-  account_id = var.cloudflare_account_id
-  name       = var.cloudflare_zone
-}
-resource "cloudflare_record" "gloomhaven_companion" {
-  count = 4
-  zone_id = data.cloudflare_zone.gloomhaven_companion.id
-  name    = var.domain_name
-  value = element(aws_route53_zone.gloomhaven_companion.name_servers, count.index)
-
-  type    = "NS"
-
-}
